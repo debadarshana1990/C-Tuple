7c2f0afb42a43bcd4fea57f018b23b9f7e3f0955
Debadarshana Parida <65441243+debadarshana1990@users.noreply.github.com>
2021-05-28 10:52:40 +0530

    Remove functinalities added

--- tuple.c
+++ tuple.c
@@ -2,7 +2,7 @@
  * tuple.c
  *
  *  Created on: 26-May-2021
- *      Author: Debadarshana Parida
+ *      Author: pde5cob
  */
 
 #include <tuple.h>
@@ -178,16 +178,18 @@ int main()
    Tuple obj;
    Tuple obj1;
    memset(&obj,0,sizeof(obj));
-   memset(&obj1,0,sizeof(obj1));
-   TupleAdd(&obj1,INT,7,INT,8,FLOAT,3.5,STR,"obj1",END);
+   //memset(&obj1,0,sizeof(obj1));
+   //TupleAdd(&obj1,INT,7,INT,8,FLOAT,3.5,STR,"obj1",END);
    //AddMember2Tuple(&obj,STR,"Deba",INT,5,INT,10,END);
-   TupleAdd(&obj,STR,"obj",INT,1,INT,5,FLOAT,3.2,DOUBLE,8.4,END);
+   //TupleAdd(&obj,STR,"obj",INT,1,INT,5,FLOAT,3.2,DOUBLE,8.4,END);
 //   AddMember2Tuple(&obj,FLOAT,3.2,DOUBLE,8.0,END);
    //printf("length:%d\n",len(obj));
-   TupleAdd(&obj,TUPLE,obj1,END);
+   //TupleAdd(&obj,TUPLE,obj1,END);
+   TupleAdd(&obj,STR,"obj",INT,1,INT,5,FLOAT,3.2,DOUBLE,8.4,END);
    TupleShow(&obj);
-   printf("length :%d",TupleLen(&obj));
+   printf("length :%d\n",TupleLen(&obj));
+   TupleRemove(&obj,STR,"obj");
+   TupleRemove(&obj,INT,5);
+   TupleShow(&obj);
    return 0;
 }
-
-
